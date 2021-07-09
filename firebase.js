0b5541e436d30fe7c2f4e0c1987299c2c7b007c4
Woody Gelius <gelius24@hotmail.com>
2021-07-08 22:36:58 -0400

    env variables changes

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtkN3imYj7qMzAolikT2pHHsteWolve_s",
+    authDomain: "wg-f3d68.firebaseapp.com",
+    projectId: "wg-f3d68",
+    storageBucket: "wg-f3d68.appspot.com",
+    messagingSenderId: "509115725872",
+    appId: "1:509115725872:web:f3511612693664d9aa55f5"
+  };
