534dd04e6681381133c1fbb99fc9dd618875306d
Woody Gelius <gelius24@hotmail.com>
2021-07-08 22:24:07 -0400

    checkout page and auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,21 @@ import {
   SearchIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import {signIn, signOut, useSession} from 'next-auth/client'
+import {useRouter} from 'next/router'
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
+  const [session] = useSession()
+  const router = useRouter()
+  const items = useSelector(selectItems)
+
   return (
-    <header className="sticky absolute top-0 z-50">
+    <header className="sticky top-0 z-50">
       <div className="flex items-center bg-amazon_blue">
         <div className="flex mt-2 items-center flex-grow sm:flex-grow-0">
-          <div>
+          <div className='cursor-pointer' onClick={() => router.push('/')}>
             <h1 className="text-white ml-1 p-1 pb-0 flex-grow py-2">AMAZON</h1>
           <p className="text-xs text-white ml-1 p-1 pt-0 pb-5 flex-grow ">Clone</p>
           </div>
@@ -24,17 +32,19 @@ function Header() {
           <SearchIcon className="h-12 p-4" />
         </div>
         <div className="flex items-center text-xs space-x-6 mx-6 whitespace-nowrap text-white">
-          <div className="link">
-            <p>Hello Woody Gelius</p>
+          <div className="link" onClick={!session ? signIn : signOut}>
+            <p>
+              {session ? `Hello ${session.user.name}` : 'Connecte toi'}
+            </p>
             <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
           <div className="link">
             <p>Returns</p>
             <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="relative link flex items-center">
+          <div className="relative link flex items-center" onClick={() => router.push('/checkout')}>
             <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
-              8
+              {items.length}
             </span>
             <ShoppingCartIcon className="h-10" />
             <p className="hidden md:inline font-extrabold md:text-sm mt-2">
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,41 +1,66 @@
-import Image from 'next/image'
-import { useState } from 'react'
-import {StarIcon} from '@heroicons/react/solid'
-import Currency from 'react-currency-formatter'
+import Image from "next/image";
+import { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
-function Product({id, title, price, description, category, image}) {
-    const [rating] = useState(Math.floor(Math.random() * (5 - 0 + 1)) + 0 )
-    const [hasPrime] = useState(Math.random() < 0.5)
+function Product({ id, title, price, description, category, image }) {
+  const [rating] = useState(Math.floor(Math.random() * (5 - 0 + 1)) + 0);
+  const [hasPrime] = useState(Math.random() < 0.5);
+  const dispatch = useDispatch();
 
-    return (
-        <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
-            <p className='absolute top-2 right-2 text-xs italic text-gray-400'>{category}</p>
-            <Image 
-            src={image} height={200} width={200} objectFit='contain' />
-            <h4 className='my-3'>{title}</h4>
-            <div className='flex'>
-                {Array(rating).fill().map((_, i) => (
-                    <StarIcon className='h-5 text-yellow-500' />
-                ))}
-            </div>
-            
-            <p className='text-xs my-2 line-clamp-2'>{description}</p>
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      rating,
+      description,
+      category,
+      image,
+      hasPrime
+    };
+    dispatch(addToBasket(product))
+  };
 
-            <div className='mb-5'>
-                <Currency quantity={price} />
-            </div>
+  return (
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">
+        {category}
+      </p>
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      <h4 className="my-3">{title}</h4>
+      <div className="flex">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5 text-yellow-500" />
+          ))}
+      </div>
 
-            {hasPrime && (
-                <div className='flex items-center space-x-2 -mt-5'>
-                    <img className='w-12' src="https://links.papareact.com/fdw" alt="prime logo" />
-                    <p className='text-xs text-gray-400'>FREE Next-day Delivery</p>
-                </div>
-            )}
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
 
-            <button className='mt-auto button'>Add to basket</button>
+      <div className="mb-5">
+        <Currency quantity={price} />
+      </div>
 
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5">
+          <img
+            className="w-12"
+            src="https://links.papareact.com/fdw"
+            alt="prime logo"
+          />
+          <p className="text-xs text-gray-400">FREE Next-day Delivery</p>
         </div>
-    )
+      )}
+
+      <button onClick={addItemToBasket} className="mt-auto button">
+        Add to basket
+      </button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
