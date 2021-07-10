4708afad9b70f06f3f4b7d9bbe3992168e1be362
gelius24 <86559389+gelius24@users.noreply.github.com>
2021-07-10 07:54:13 -0400

    Update create-checkout-session.js

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -2,7 +2,7 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
-  const { items, email } = req.body;
+  const { items, email } = req.rawBody;
 
   const transformedItems = items.map((item) => ({
     description: item.description,
