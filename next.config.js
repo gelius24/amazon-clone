faf1362dd7f8e1476a1ead8360c2993428b9940e
gelius24 <86559389+gelius24@users.noreply.github.com>
2021-07-10 11:02:24 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     domains: ["links.papareact.com", "fakestoreapi.com"],
   },
   env: {
-    stripe_public_key: process.env.STRIPE_PUBLIC_KEY
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+    host_url: process.env.HOST
   }
 };
